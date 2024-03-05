9d57154ec9245a8b31ff055f350964a2798446c7
Raunak Scarlet <raunakscarlet@gmail.com>
2024-03-05 18:42:13 +0530

    setup models and cloudinary

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
 dotenv.config({
   path: "./.env",
 });
+const port = process.env.PORT;
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Server Connection error`, err);
+  });
